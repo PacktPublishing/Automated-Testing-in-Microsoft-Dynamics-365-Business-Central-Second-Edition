91089fbe025e0bf29952112701fd8f086e2e8042
Luc van Vugt <lvanvugt@live.nl>
2021-06-05 11:14:32 +0200

    Moved DisabledTests.json and added conversion script

---
+++ Chapter 10 (LookupValue Extension)/disabledtests/ConvertTo-DisabledTests.ps1
@@ -0,0 +1,22 @@
+﻿function ConvertTo-DisabledTests
+{
+    param(
+        [Parameter()]
+        [String] $inputFolderPath = '',
+
+        [Parameter()]
+        [String] $inputFileName = 'TestResults.xml',
+
+        [Parameter()]
+        [string] $outputFileName = 'DisabledTests.json'
+    )
+
+    $inputFilePath = join-path $inputFolderPath $inputFileName
+    $outputFilePath = join-path $inputFolderPath $outputFileName
+
+    $disabledTestsJSON = @()
+    $results = [xml](Get-Content -Path $inputFilePath)
+    $results.SelectNodes('//test') | Where-Object { $_.Result -ne "Pass" } | ForEach-Object { $disabledTestsJSON += [ordered]@{ "codeunitName" = $_.name.Split(':')[0]; "method" = $_.method } }
+
+    $disabledTestsJSON | ConvertTo-Json -Depth 99 | Set-Content $outputFilePath
+}
