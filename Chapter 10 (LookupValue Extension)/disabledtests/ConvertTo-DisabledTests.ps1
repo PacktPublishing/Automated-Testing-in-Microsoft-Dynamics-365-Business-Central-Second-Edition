f2530d34c1108043fed76729c6a61bc4e72e9ff8
Luc van Vugt <lvanvugt@live.nl>
2021-06-05 12:01:35 +0200

    Removed function container from ConvertTo-DisabledTests.ps1

--- Chapter 10 (LookupValue Extension)/disabledtests/ConvertTo-DisabledTests.ps1
+++ Chapter 10 (LookupValue Extension)/disabledtests/ConvertTo-DisabledTests.ps1
@@ -1,25 +1,22 @@
-﻿function ConvertTo-DisabledTests
-{
-    param(
-        [Parameter()]
-        [String] $inputFolderPath = '',
+﻿param(
+    [Parameter()]
+    [String] $inputFolderPath = '',
 
-        [Parameter()]
-        [String] $inputFileName = 'TestResults.xml',
+    [Parameter()]
+    [String] $inputFileName = 'TestResults.xml',
 
-        [Parameter()]
-        [string] $outputFileName = 'DisabledTests.json'
-    )
+    [Parameter()]
+    [string] $outputFileName = 'DisabledTests.json'
+)
 
-    write-host "ConvertTo-DisabledTests"
-    $inputFilePath = join-path $inputFolderPath $inputFileName
-    write-host "Input file: $inputFilePath"
-    $outputFilePath = join-path $inputFolderPath $outputFileName
-    write-host "Output file: $outputFilePath"
+write-host "ConvertTo-DisabledTests"
+$inputFilePath = join-path $inputFolderPath $inputFileName
+write-host "Input file: $inputFilePath"
+$outputFilePath = join-path $inputFolderPath $outputFileName
+write-host "Output file: $outputFilePath"
 
-    $disabledTestsJSON = @()
-    $results = [xml](Get-Content -Path $inputFilePath)
-    $results.SelectNodes('//test') | Where-Object { $_.Result -ne "Pass" } | ForEach-Object { $disabledTestsJSON += [ordered]@{ "codeunitName" = $_.name.Split(':')[0]; "method" = $_.method } }
+$disabledTestsJSON = @()
+$results = [xml](Get-Content -Path $inputFilePath)
+$results.SelectNodes('//test') | Where-Object { $_.Result -ne "Pass" } | ForEach-Object { $disabledTestsJSON += [ordered]@{ "codeunitName" = $_.name.Split(':')[0]; "method" = $_.method } }
 
-    $disabledTestsJSON | ConvertTo-Json -Depth 99 | Set-Content $outputFilePath
-}
+$disabledTestsJSON | ConvertTo-Json -Depth 99 | Set-Content $outputFilePath
