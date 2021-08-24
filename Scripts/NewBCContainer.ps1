c395b838b812bea6e76cb3ee8624415c793ea250
Luc van Vugt <lvanvugt@live.nl>
2021-08-24 18:27:50 +0200

    Added PowerShell scripts to go with Appendix

---
+++ Scripts/NewBCContainer.ps1
@@ -0,0 +1,28 @@
+﻿#Install-Module -Name bccontainerhelper -force  
+
+# set accept_eula to $true to accept the eula found here: https://go.microsoft.comfwlink/?linkid=861843
+$accept_eula = $true
+
+# set container name and version
+$containerName = 'your-container-name'
+
+# set artifact url
+$artifactUrl = Get-BCArtifactUrl -country "base" -version "19"
+
+# set credentials for UserPassword usage
+$login = 'your-user-name'
+$password = 'your-password'
+$securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object PSCredential($login, $securepassword)
+
+New-BCContainer -accept_eula:$accept_eula `
+                -containerName $containerName `
+                -artifactUrl $artifactUrl `
+                -auth UserPassword `
+                -credential $credential `
+                -memoryLimit:10GB `
+                -updateHost `
+                -shortcuts StartMenu `
+                -isolation process `
+                -includeTestToolkit `
+                -includeTestLibrariesOnly
