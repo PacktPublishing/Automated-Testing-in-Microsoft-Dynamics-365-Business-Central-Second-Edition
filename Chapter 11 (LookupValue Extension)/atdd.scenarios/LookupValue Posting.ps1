d8573f907dc7cc6ce8b5650e5ee3531dfccb0d8a
Luc van Vugt <lvanvugt@live.nl>
2021-08-28 13:11:13 +0200

    Adjusted LooukupValue Posting due to ATTD.TestScriptor example

--- Chapter 11 (LookupValue Extension)/atdd.scenarios/LookupValue Posting.ps1
+++ Chapter 11 (LookupValue Extension)/atdd.scenarios/LookupValue Posting.ps1
@@ -1,30 +1,30 @@
 Feature 'LookupValue Posting' {
-    Scenario 0022 'Check that posted sales invoice and shipment inherit lookup value from sales order' {
-        Given 'Sales order with Lookup value'
-        When 'Sales order is posted (invoice & ship)'
+    Scenario 0022 'Posted sales invoice and shipment inherit lookup value from sales order' {
+        Given 'Sales order with lookup value'
+        When 'Post sales order (invoice & ship)'
         Then 'Posted sales invoice has lookup value from sales order'
         Then 'Sales shipment has lookup value from sales order'
     }
 
-    Scenario 0027 'Check posting throws error on sales order with empty lookup value' {
-        Given 'Sales order without Lookup value'
-        When 'Sales order is posted (invoice & ship)'
+    Scenario 0027 'Posting throws error on sales order with empty lookup value' {
+        Given 'Sales order without lookup value'
+        When 'Post sales order (invoice & ship)'
         Then 'Missing lookup value on sales order error thrown'
     }
 
-    Scenario 0023 'Check that posted warehouse shipment line inherits lookup value from sales order through warehouse shipment line' {
+    Scenario 0023 'Posted warehouse shipment line inherits lookup value from sales order' {
         Given 'Location with require shipment'
         Given 'Warehouse employee for current user'
-        Given 'Warehouse shipment line with Lookup value created from Sales order'
-        When 'Warehouse shipment is posted'
-        Then 'Posted warehouse shipment line has lookup value from warehouse shipment line'
+        Given 'Warehouse shipment line from sales order with lookup value'
+        When 'Post Warehouse shipment'
+        Then 'Posted warehouse shipment line has lookup value from sales order'
     }
 
-    Scenario 0025 'Check posting throws error on sales order with empty lookup value' {
+    Scenario 0025 'Posting throws error on warehouse shipment line with empty lookup value' {
         Given 'Location with require shipment'
         Given 'Warehouse employee for current user'
-        Given 'Warehouse shipment line created from Sales order without  lookup value'
-        When 'Warehouse shipment is posted'
+        Given 'Warehouse shipment line from sales order without lookup value'
+        When 'Post Warehouse shipment'
         Then 'Missing lookup value on sales order error thrown'
     }
 }    
