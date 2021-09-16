ad98e517318fc057b97dccc2266ce6a421c6e707
Luc van Vugt <lvanvugt@live.nl>
2021-09-16 16:37:28 +0200

    SQL script to remove all user related data

---
+++ Scripts/RemoveAllUserRelatedData.sql
@@ -0,0 +1,29 @@
+/* Query to Remove all user related data to enable testing of APIs - see chapter 13 */
+
+USE [Demo Database BC (18-0)]
+GO
+
+SELECT *  FROM [dbo].[User]
+DELETE FROM [dbo].[User]
+SELECT *  FROM [dbo].[User]
+GO
+
+SELECT *  FROM [dbo].[User Property]
+DELETE FROM [dbo].[User Property]
+SELECT *  FROM [dbo].[User Property]
+GO
+
+SELECT *  FROM [dbo].[Page Data Personalization]
+DELETE FROM [dbo].[Page Data Personalization]
+SELECT *  FROM [dbo].[Page Data Personalization]
+GO
+
+SELECT *  FROM [dbo].[User Personalization]
+DELETE FROM [dbo].[User Personalization]
+SELECT *  FROM [dbo].[User Personalization]
+GO
+
+SELECT *  FROM [dbo].[Access Control]
+DELETE FROM [dbo].[Access Control]
+SELECT *  FROM [dbo].[Access Control]
+GO
