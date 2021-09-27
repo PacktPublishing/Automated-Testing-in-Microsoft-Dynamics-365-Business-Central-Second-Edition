3c289a1a3205614e4bd9030942c506962f1a5f03
Luc van Vugt <lvanvugt@live.nl>
2021-09-27 21:00:57 +0200

    Third version of API tests

---
+++ Scripts/Run-TestsInBcContainer.ps1
@@ -0,0 +1,12 @@
+﻿# set container name and version
+$containerName = 'your-container-name'
+
+# set credentials for Windows usage
+$login = 'your-windows-user-name'
+$password = 'your-password'
+$securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object PSCredential($login, $securepassword)
+
+Run-TestsInBcContainer -containerName $containerName `
+                       -credential $credential `
+                       -extensionId 'e26890f8-fafe-49c6-8951-2c1457921f9b' `                       -detailed `                       -testPage 130455 `                       -testRunnerCodeunitId 130451
